b5d8979a115f3c47793cd346567de68fc89234b8
mt-toon <jtoon@moneytree.jp>
2018-02-06 12:59:59 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # mt-web-icons
+# Setup
+```
+yarn install
+```
+
+# Steps for updating the web font
+1. Go to https://icomoon.io/app/#/select
+2. Upload the latest ``selection.json`` in the ``iconmoon`` folder
+3. In the iconmoon web app, just upload whatever svg you want to add. Be sure to center it and name it properly.
+4. Generate the new web font
+5. Download it
+6. Unzip it
+7. Copy the whole unzipped folder to update this repository
---
+++ icomoon/ie7/ie7.js
@@ -0,0 +1,44 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'moneytree\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'mt-logotype': '&#xe900;',
+		'mt-logo': '&#xe901;',
+		'mt-nub-left': '&#xe902;',
+		'mt-nub-right': '&#xe903;',
+		'mt-nub-up': '&#xe904;',
+		'mt-nub-down': '&#xe905;',
+		'mt-question': '&#xe906;',
+		'mt-chevron-left': '&#xe907;',
+		'mt-chevron-right': '&#xe908;',
+		'mt-chevron-up': '&#xe909;',
+		'mt-chevron-down': '&#xe90a;',
+		'mt-back': '&#xe90b;',
+		'mt-forward': '&#xe90c;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/mt-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
